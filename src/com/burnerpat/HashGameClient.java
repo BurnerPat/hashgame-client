c4789eb7baedfaebf9853d08fadb9a5f4497face
BurnerPat <burner.patrick@googlemail.com>
2014-05-15 17:54:46 +0200

    * initial commit

---
+++ src/com/burnerpat/HashGameClient.java
@@ -0,0 +1,186 @@
+package com.burnerpat;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HashGameClient {
+	
+	private static final String SERVER_URL = "http://hash.h10a.de/";
+	private static String USERNAME = "BurnerPat";
+	private static int THREAD_COUNT = 10;
+	private static String HASH = "";
+	
+	private static HashGameWorker[] threads = null;
+	private static boolean temp = false;
+	
+	public static void main(String[] args) {		
+		if (args.length != 2) {
+			System.out.println("Please provide the username and number of threads");
+			return;
+		}
+		
+		USERNAME = args[0];
+		
+		try {
+			THREAD_COUNT = Integer.parseInt(args[1]);
+		}
+		catch (NumberFormatException ex) {
+			System.out.println("Pleas provide a valid nunber");
+			return;
+		}
+		
+		start();
+	}
+	
+	private static String retrieveHash() {
+		List<String> hashes = new ArrayList<String>();
+		List<Integer> chains = new ArrayList<Integer>();
+		
+		try {
+			InputStream in = new URL(SERVER_URL + "?raw").openStream();
+			
+			while (true) {
+				byte[] buffer = new byte[64];
+				
+				if (in.read(buffer) < 0) {
+					break;
+				}
+				
+				hashes.add(new String(buffer));
+				
+				in.read();
+				
+				int len = 0;
+				int c = 0;
+				while ((c = in.read()) >= 0 && (!Character.isWhitespace(c))) {
+					len *= 10;
+					len += (char)c - '0';
+				}
+				
+				chains.add(len);
+			}
+			
+			in.close();
+		}
+		catch (IOException ex) {
+			System.err.println("Failed to request current hashes");
+			ex.printStackTrace(System.err);
+			return null;
+		}
+		
+		int max = 0;
+		String hash = "";
+		for (int i = 0; i < hashes.size(); i++) {
+			if (chains.get(i) > max) {
+				max = chains.get(i);
+				hash = hashes.get(i);
+			}
+		}
+		
+		return hash;
+	}
+	
+	private static void start() {
+		HASH = retrieveHash();
+		
+		if (HASH == null) {
+			System.err.println("Failed to fetch longest hash. Shutting down.");
+			System.exit(1);
+			return;
+		}
+		
+		System.out.println("Using hash: " + HASH);
+		
+		startWorkers(HASH);
+		
+		if (!UPDATER.isAlive()) {
+			UPDATER.start();
+		}
+	}
+	
+	private static void startWorkers(String hash) {
+		System.out.println("Starting " + THREAD_COUNT + " worker threads...");
+		threads = new HashGameWorker[THREAD_COUNT];
+		
+		for (int i = 0; i < THREAD_COUNT; i++) {
+			threads[i] = new HashGameWorker(hash, USERNAME, listener);
+			threads[i].start();
+		}
+	}
+
+	private static final HashGameWorker.Listener listener = new HashGameWorker.Listener() {
+		@Override
+		public void notifySuccess(String hash, String seed, String parent) {
+			if (temp == true) {
+				return;
+			}
+			
+			System.out.println("Found hash: " + hash);
+			System.out.println("Seed: " + seed);
+			System.out.println("Stopping workers...");
+			temp = true;
+			for (HashGameWorker thread : threads) {
+				thread.interrupt();
+			}
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+			}
+			
+			try {
+				InputStream in = new URL(SERVER_URL + "?Z=" + parent + "&P=" + USERNAME + "&R=" + seed).openStream();
+				while (in.read() >= 0) {
+					
+				}
+				in.close();
+			}
+			catch (IOException ex) {
+				System.err.println("Failed to post new hash");
+				ex.printStackTrace(System.err);
+			}
+			
+			start();
+		}
+	};
+	
+	private static Thread UPDATER = new Thread(){
+		public void run() {
+			try {
+				while (true) {
+					sleep(10000);
+					
+					String hash = retrieveHash();
+					
+					if (hash == null) {
+						System.err.println("Failed to retrieve update! Shutting down.");
+						System.exit(1);
+						return;
+					}
+					
+					if (hash.equals(HASH)) {
+						continue;
+					}
+					
+					System.out.println("Found update, stopping workers");
+					HASH = hash;
+					
+					for (HashGameWorker thread : threads) {
+						thread.interrupt();
+					}
+					
+					sleep(1000);
+					
+					startWorkers(HASH);
+				}
+			}
+			catch (InterruptedException ex) {
+				
+			}
+		};
+	};
+}
---
+++ src/com/burnerpat/HashGameWorker.java
@@ -0,0 +1,60 @@
+package com.burnerpat;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+
+public class HashGameWorker extends Thread {
+	private final String parent;
+	private final String user;
+	private final Listener listener;
+	private static Random random = new Random();
+	private static long lastUpdate = 0;
+	
+	public HashGameWorker(String parent, String user, Listener listener) {
+		this.parent = parent;
+		this.user = user;
+		this.listener = listener;
+	}
+	
+	@Override
+	public void run() {
+		MessageDigest digest = null;
+		
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		}
+		catch (NoSuchAlgorithmException ex) {
+			System.err.println("Failed to retrieve digest");
+			ex.printStackTrace(System.err);
+			return;
+		}
+		
+		while (!isInterrupted()) {
+			if (lastUpdate < System.currentTimeMillis()) {
+				lastUpdate = System.currentTimeMillis() + 100;
+				random.setSeed(lastUpdate + random.nextLong());
+			}
+			
+			final String seed = Long.toHexString(random.nextLong());
+			final String line = parent + " " + user + " " + seed;
+			final byte[] hash = digest.digest(line.getBytes());
+			
+			if (hash[0] == 0 &&
+				hash[1] == 0 &&
+				hash[2] == 0 &&
+				(hash[3] & 0xF0) == 0) {
+				
+				new Thread() {
+					public void run() {
+						listener.notifySuccess(javax.xml.bind.DatatypeConverter.printHexBinary(hash).toLowerCase(), seed, parent);
+					}
+				}.start();
+			}
+		}
+	}
+	
+	public interface Listener {
+		public void notifySuccess(String hash, String seed, String parent);
+	}
+}
